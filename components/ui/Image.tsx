f6d534bef6c2ac9f2ccb1d4f220ab67981184aea
Vaibhav Acharya <vaibhav@gosupernova.live>
2024-12-01 21:10:11 +0530

    more deps

--- components/ui/Image.tsx
+++ components/ui/Image.tsx
@@ -1,21 +1,12 @@
-import { Image as ExpoImage } from "expo-image";
-import React from "react";
-import { StyleProp, ImageStyle } from "react-native";
-import { cn } from "@/utils/styling";
+import { Image as ExpoImage, type ImageProps } from "expo-image";
+import { cssInterop } from "nativewind";
 
-export type ImageProps = {
-  className?: string;
-  style?: StyleProp<ImageStyle>;
-} & Omit<React.ComponentProps<typeof ExpoImage>, "style">;
+const ImagePrimitive = cssInterop(ExpoImage, {
+  className: {
+    target: "style",
+  },
+});
 
-export function Image({ className, style, ...props }: ImageProps) {
-  return (
-    <ExpoImage
-      className={cn(className)}
-      style={style}
-      contentFit={props.contentFit ?? "cover"}
-      transition={props.transition ?? 200}
-      {...props}
-    />
-  );
+export function Image({ ...props }: ImageProps) {
+  return <ImagePrimitive {...props} />;
 }
